e2fdd162740ffc2cef51b6c321a8ea1827095b33
Saqib Siddique <saqibsiddique@Macs-MacBook-Pro.local>
2023-12-13 13:09:29 +0500

    initial Commit

---
+++ PopularMovies/PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by Saqib Siddique on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
